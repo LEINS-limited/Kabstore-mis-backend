bb1cb56ee3d2f6150b204eecd881fab83de91de3
U-Alice <umugwanezaalice22@gmail.com>
2025-01-29 12:28:05 +0200

    [fix]: Error handling

--- src/filters/global-exception.filter.ts
+++ src/filters/global-exception.filter.ts
@@ -13,13 +13,16 @@ export class GlobalExceptionFilter implements ExceptionFilter {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
     const request = ctx.getRequest<Request>();
-    console.log("Error occured",exception);
+    console.log("Error occured",exception.response);
     const status =
       exception instanceof HttpException
         ? exception.getStatus()
         : HttpStatus.INTERNAL_SERVER_ERROR;
 
-    let message = exception instanceof HttpException ? exception.message : 'Internal server error';
+    let message = exception.response.message;
+    if(!(exception instanceof HttpException)){
+      message = "Internal server error!";
+    }
     if (message.includes("Expected ',' or '}' after property value in JSON")) {
       message = "Invalid JSON format in request body. Please check your JSON syntax.";
     }
