832bdbe377da405e16b66b460980dd1767745c86
Hallcoder <hallcoder25@gmail.com>
2025-11-15 22:01:41 +0200

    fix: private key test

--- src/integrations/mailing/mailing.service.ts
+++ src/integrations/mailing/mailing.service.ts
@@ -81,6 +81,7 @@ export class MailingService {
           html: `${headerHTML}
             </div>
             <div class='content'>
+            
               <p>Hello ${recipient.lastName},</p>
               <p>Welcome to Kabstore. Please verify your email to get started.</p>
               <p>Your verification code:</p>
